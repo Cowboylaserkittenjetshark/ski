0c7bdce998b32f304e72ab204c58e2216a58025a
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2025-05-12 22:07:02 -0400

    feat: init with Hello

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "A simple tool to switch active ssh key pairs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        craneLib = crane.mkLib pkgs;
+
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+
+          buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.libiconv
+          ];
+        };
+
+        ski = craneLib.buildPackage (commonArgs // {
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        });
+      in
+      {
+        checks = {
+          inherit ski;
+        };
+
+        packages.default = ski;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = ski;
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+          packages = with pkgs; [
+            nil
+            statix
+            deadnix
+            rust-analyzer
+            cargo-generate
+          ];
+        };
+      });
+}
