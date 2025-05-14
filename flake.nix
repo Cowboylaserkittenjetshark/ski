6b76d13390f257760aa8f018d34b69b257b9b3cf
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2025-05-14 03:31:27 -0400

    fix (nix): again

--- flake.nix
+++ flake.nix
@@ -63,47 +63,6 @@
         ];
       };
 
-      homeModules.default = inputs: {config, lib, pkgs, ...}: let
-        cfg = config.programs.ski;
-        inherit (lib) mkOption mkEnableOption mkIf types;
-        inherit (pkgs.stdenv.hostPlatform) system;
-        tomlFormat = pkgs.formats.toml {};
-      in {
-        options.programs.ski = {
-          enable = mkEnableOption "ski";
-
-          settings = mkOption {
-            type = tomlFormat.type;
-            default = {};
-            example = {
-              pairs = {
-                k1.name = "k1_1234_ed25519";
-                k2 = {
-                  public = "/a/path/outside/of/.ssh/k2_4321.pub";
-                  private = "/a/path/outside/of/.ssh/k2_4321";
-                };
-              };
-              roles = {
-                auth.target = "id_ed25519_sk";
-                sign.target = "signing-key";
-              };
-            };
-            description = "Options to add to the {file}`ski.toml` file";
-          };
-          
-          package = mkOption {
-            type = types.package;
-            default = inputs.self.packages.${system}.ski;
-            description = "The ski package to use.";
-          };
-        };
-        config = mkIf cfg.enable {
-          home.packages = [cfg.package];
-
-          home.file.".ssh/config.toml" = mkIf (cfg.settings != {}) {
-            source = tomlFormat.generate "ski-config" cfg.settings;
-          };
-        };
-      } inputs;
+      homeModules.default = import ./nix/module.nix inputs;
     });
 }
---
+++ nix/module.nix
@@ -0,0 +1,42 @@
+inputs: {config, lib, pkgs, ...}: let
+        cfg = config.programs.ski;
+        inherit (lib) mkOption mkEnableOption mkIf mkDefault types;
+        inherit (pkgs.stdenv.hostPlatform) system;
+        tomlFormat = pkgs.formats.toml {};
+      in {
+        options.programs.ski = {
+          enable = mkEnableOption "ski";
+
+          settings = mkOption {
+            type = tomlFormat.type;
+            default = {};
+            example = {
+              pairs = {
+                k1.name = "k1_1234_ed25519";
+                k2 = {
+                  public = "/a/path/outside/of/.ssh/k2_4321.pub";
+                  private = "/a/path/outside/of/.ssh/k2_4321";
+                };
+              };
+              roles = {
+                auth.target = "id_ed25519_sk";
+                sign.target = "signing-key";
+              };
+            };
+            description = "Options to add to the {file}`ski.toml` file";
+          };
+          
+          package = mkOption {
+            type = types.package;
+            description = "The ski package to use.";
+          };
+        };
+        config = mkIf cfg.enable {
+          home.packages = [cfg.package];
+          programs.ski.package = mkDefault inputs.self.packages.${system}.ski;
+
+          home.file.".ssh/config.toml" = mkIf (cfg.settings != {}) {
+            source = tomlFormat.generate "ski-config" cfg.settings;
+          };
+        };
+      } inputs
